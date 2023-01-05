bf3323e50590d015009c00f7c5b2db271936c8cd
marosan28 <danijela.marosan36@gmail.com>
2023-01-05 18:06:53 +0000

    change secret key value

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 os.environ["DATABASE_URL"] = "postgres://rvqyyjhw:vL3wfBVBF16q3tB-WWLP_OpnVSU5JXxx@tai.db.elephantsql.com/rvqyyjhw"
-os.environ["SECRET_KEY"] = "a0venye_dc+&ij^ph#kpwdt^mlngr(%mq847w6-2lo*voz&f6q"
+os.environ["SECRET_KEY"] = "4&v-i=#jaiolacfkt$4)&mjnghgvq5vj3o&sx_e1xnpm^7r-cm"
 os.environ["CLOUDINARY_URL"] = "cloudinary://522695269739824:oa4_FiNurIBFcShGyu3gXGAEpeM@dd9o1h7oh"
