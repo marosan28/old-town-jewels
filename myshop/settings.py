5a1dc2fd9994a4a5f4bc09f2f1404c1af1d60924
marosan28 <danijela.marosan36@gmail.com>
2023-01-05 14:29:44 +0000

    deployment commit

--- myshop/settings.py
+++ myshop/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -64,7 +65,7 @@ ROOT_URLCONF = 'myshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-         'DIRS': [TEMPLATES_DIR],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- old-town-jewels/env.py
+++
@@ -1,4 +0,0 @@
-import os
-os.environ["DATABASE_URL"] = "postgres://rvqyyjhw:vL3wfBVBF16q3tB-WWLP_OpnVSU5JXxx@tai.db.elephantsql.com/rvqyyjhw"
-os.environ["SECRET_KEY"] = "a0venye_dc+&ij^ph#kpwdt^mlngr(%mq847w6-2lo*voz&f6q"
-os.environ["CLOUDINARY_URL"] = "cloudinary://522695269739824:oa4_FiNurIBFcShGyu3gXGAEpeM@dd9o1h7oh"
