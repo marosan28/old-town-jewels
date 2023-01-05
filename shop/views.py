a99624d530360522a834ad257b90246b723679f6
marosan28 <danijela.marosan36@gmail.com>
2023-01-05 15:46:06 +0000

    add product detail to views

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+
+
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
@@ -13,3 +15,13 @@ def product_list(request, category_slug=None):
                   {'category': category,
                    'categories': categories,
                    'products': products})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product})
