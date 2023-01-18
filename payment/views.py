5d74f6b3b68a15f7fad0a74bdf65eb28746f85c3
marosan28 <danijela.marosan36@gmail.com>
2023-01-18 20:48:30 +0000

    change currency to eur

--- payment/views.py
+++ payment/views.py
@@ -33,7 +33,7 @@ def payment_process(request):
             session_data['line_items'].append({
                 'price_data': {
                     'unit_amount': int(item.price * Decimal('100')),
-                    'currency': 'usd',
+                    'currency': 'eur',
                     'product_data': {
                         'name': item.product.name,
                     },
@@ -53,8 +53,6 @@ def payment_process(request):
 
 def payment_completed(request):
     return render(request, 'payment/completed.html')
-
-
 def payment_canceled(request):
     return render(request, 'payment/canceled.html')
 
