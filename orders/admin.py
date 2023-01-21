7e9f2050aefd0c2cac70cec6bf547af1dca47353
marosan28 <danijela.marosan36@gmail.com>
2023-01-21 16:06:15 +0000

    edit decorator typo mistake in admin py

--- orders/admin.py
+++ orders/admin.py
@@ -1,11 +1,21 @@
+from django.contrib import admin
+from .models import Order, OrderItem
 import csv
 import datetime
 from django.http import HttpResponse
-from django.contrib import admin
-from django.urls import reverse
 from django.utils.safestring import mark_safe
-from .models import Order, OrderItem
 
+def order_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+order_payment.short_description = 'Stripe payment'
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
 
 def export_to_csv(modeladmin, request, queryset):
     opts = modeladmin.model._meta
@@ -13,8 +23,8 @@ def export_to_csv(modeladmin, request, queryset):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = content_disposition
     writer = csv.writer(response)
-    fields = [field for field in opts.get_fields() if not
-              field.many_to_many and not field.one_to_many]
+    fields = [field for field in opts.get_fields() if not \
+              field.many_to_many and not field.one_to_many] 
     # Write a first row with header information
     writer.writerow([field.verbose_name for field in fields])
     # Write data rows
@@ -27,38 +37,13 @@ def export_to_csv(modeladmin, request, queryset):
             data_row.append(value)
         writer.writerow(data_row)
     return response
-
-
-export_to_csv.short_description = 'Export to CSV'
-
-
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    raw_id_fields = ['product']
-
-
-def order_payment(obj):
-    url = obj.get_stripe_url()
-    if obj.stripe_id:
-        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
-        return mark_safe(html)
-    return ''
-
-
-order_payment.short_description = 'Stripe payment'
-
-
-def order_detail(obj):
-    url = reverse('orders:admin_order_detail', args=[obj.id])
-    return mark_safe(f'<a href="{url}">View</a>')
-
+export_to_csv.short_description = 'Export to CSV' 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    order_payment, 'created', 'updated',
-                    order_detail]
+                    order_payment,'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
