29f3f01c82c82ad02a7cb7cfbeb5bc9a09f8ebfc
marosan28 <danijela.marosan36@gmail.com>
2023-01-23 16:24:04 +0000

    create forms.py in users

---
+++ users/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class Login(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput())
