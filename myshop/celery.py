be56ac510153de4d8f31576c42376a30b01ce53b
marosan28 <danijela.marosan36@gmail.com>
2023-01-24 07:38:58 +0000

    add environment variables for celery and rabbitmq

--- myshop/celery.py
+++ myshop/celery.py
@@ -1,7 +1,11 @@
 import os
 from celery import Celery
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '<your_project_name>.settings')
 app = Celery('myshop')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+app.conf.update(
+    BROKER_URL=os.environ.get('CLOUDAMQP_URL'),
+    CELERY_RESULT_BACKEND=os.environ.get('CLOUDAMQP_URL')
+)
