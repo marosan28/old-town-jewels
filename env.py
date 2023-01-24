6ebb4c68fdc38999ef6d7b5f7ba232353679974a
marosan28 <danijela.marosan36@gmail.com>
2023-01-24 08:01:31 +0000

    add settings for celery and rabbit to settings.py

--- env.py
+++ env.py
@@ -1,4 +1,5 @@
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "old-town-jewels.myshop.settings")
 os.environ["DATABASE_URL"] = "postgres://rvqyyjhw:vL3wfBVBF16q3tB-WWLP_OpnVSU5JXxx@tai.db.elephantsql.com/rvqyyjhw"
 os.environ["SECRET_KEY"] = "4&v-i=#jaiolacfkt$4)&mjnghgvq5vj3o&sx_e1xnpm^7r-cm"
 os.environ["CLOUDINARY_URL"] = "cloudinary://522695269739824:oa4_FiNurIBFcShGyu3gXGAEpeM@dd9o1h7oh"
--- myshop/celery.py
+++ myshop/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '<your_project_name>.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
 app = Celery('myshop')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
