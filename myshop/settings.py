c7cf80966be5b8f1d0eb01920f5e294a25143227
marosan28 <danijela.marosan36@gmail.com>
2023-01-25 00:03:56 +0000

    change database settings to if loop

--- myshop/settings.py
+++ myshop/settings.py
@@ -95,9 +95,17 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 if development:
     ALLOWED_HOSTS = ['localhost']
