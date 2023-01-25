a7732227ea46a4038983ea4197aaf126a2a5d6de
marosan28 <danijela.marosan36@gmail.com>
2023-01-25 00:03:25 +0000

    create superuser for postgres

--- myshop/settings.py
+++ myshop/settings.py
@@ -95,17 +95,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if development:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 if development:
     ALLOWED_HOSTS = ['localhost']
