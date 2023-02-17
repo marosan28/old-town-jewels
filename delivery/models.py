43c4adbe4ef4ee6ea19faf067025ead60d45ca6f
marosan28 <danijela.marosan36@gmail.com>
2023-02-17 09:22:19 +0000

    add placeholder to delivery charge options

--- delivery/models.py
+++ delivery/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class DeliveryOption(models.Model):
-    name = models.CharField(max_length=50, default='Standard Shipping')
-    price = models.DecimalField(max_digits=6, decimal_places=2, default=5.99)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
 
