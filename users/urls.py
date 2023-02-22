e360ef740f888eafc5ed735dfcfc3564503d3049
marosan28 <danijela.marosan36@gmail.com>
2023-02-22 13:27:03 +0000

    define url patterns for views in urls.py for password change

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from .import views
-
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView, PasswordResetConfirmView
 app_name = 'users'
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
@@ -8,4 +8,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('users/logout/', views.logout_view, name='logout'),
     path('subscribe', views.subscribe, name='subscribe'),
+    path('change-password/', PasswordChangeView.as_view(), name='password_change'),
+    path('change-password/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
